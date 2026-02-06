0bd65592d6466b3523e4bbe27895d5018a41e94f
PChan-3137 <pc313711@gmail.com>
2026-02-06 14:02:37 +0900

    first commit

---
+++ JS/brandDetail.js
@@ -0,0 +1,151 @@
+import { brandData } from "./brandData.js";
+
+
+
+// URL id
+const params = new URLSearchParams(location.search);
+const brandId = params.get("id");
+
+// 현재 브랜드
+const brand = brandData.find(b => b.id === brandId);
+if (!brand) {
+  document.body.innerHTML = "<h1>브랜드를 찾을 수 없습니다.</h1>";
+  throw new Error("Brand not found");
+}
+
+// index
+const index = brandData.findIndex(b => b.id === brandId);
+
+// 기본 정보 바인딩
+document.getElementById("brandEN").textContent = brand.nameEN;
+document.getElementById("brandKR").textContent = brand.nameKR;
+document.getElementById("brandDescTitle").textContent = brand.description.title;
+document.getElementById("brandDescText").textContent = brand.description.text;
+
+// Hero Video
+const hero = document.getElementById("heroSection");
+const heroText = document.getElementById("heroText");
+const video = document.getElementById("heroVideo");
+const videoSource = document.getElementById("heroSource");
+const heroInner = document.getElementById("heroInner");
+videoSource.src = brand.heroVideo;
+video.load();
+video.muted = true;
+video.playsInline = true;
+
+let played = false;
+
+window.addEventListener("scroll", () => {
+  const rect = hero.getBoundingClientRect();
+  const triggerPoint = window.innerHeight / 2;
+  const scrollY = window.scrollY;
+  if (rect.top < triggerPoint && rect.bottom > triggerPoint) {
+    heroInner.classList.add("fullscreen");
+    if (!played) {
+      video.play();
+      played = true;
+    }
+  }
+  if (scrollY > 800) {
+    heroText.classList.add("show");
+    heroInner.classList.add("dim");
+  }
+  if (scrollY > 2000) {
+    heroText.classList.remove("show");
+    heroInner.classList.remove("fullscreen");
+  }
+});
+
+// Product Swiper
+const productWrapper = document.getElementById("productWrapper");
+
+brand.products.forEach((product, idx) => {
+  const slide = document.createElement("div");
+  slide.className = "swiper-slide";
+  slide.innerHTML = `
+    <div class="card">
+      <img src="${product.image}" alt="${product.name}" />
+    </div>
+    <p class="title">${product.name}</p>
+  `;
+  slide.addEventListener("click", () => {
+    location.href = `brand-product.html?id=${brand.id}&product=${idx}`;
+  });
+  productWrapper.appendChild(slide);
+});
+
+
+document.querySelector(".product-bg").style.backgroundImage =`url(${brand.productBg})`;
+
+
+const swiper = new Swiper(".productSwiper", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  loop: false,
+  speed: 600,
+  breakpoints: {
+    0: { slidesPerView: 3,spaceBetween: 16 },
+    768: { slidesPerView:"auto",spaceBetween: 24 },
+    1024: { spaceBetween: 40 },
+  },
+  navigation: {
+    prevEl: ".swiper-btn.prev",
+    nextEl: ".swiper-btn.next",
+  },
+});
+
+
+
+
+document.querySelector(".productSwiper").addEventListener("click", e => {
+  const slideEl = e.target.closest(".swiper-slide");
+  if (!slideEl) return;
+
+  const index = swiper.realIndex; // 항상 진짜 index
+  const product = brand.products[index];
+
+  console.log(product.name);
+});
+
+
+// Media Section
+const mediaSection = document.querySelector(".media");
+const mediaList = document.getElementById("mediaList");
+
+if (!brand.media || brand.media.length === 0) {
+  mediaSection.remove();
+} else {
+  brand.media.forEach(m => {
+    const item = document.createElement("a");
+    item.href = m.link;
+    item.target = "_blank";
+    item.innerHTML = `<img src="${m.thumb}" />`;
+    mediaList.appendChild(item);
+  });
+}
+// Prev / Next Brand
+const total = brandData.length;
+
+const prevIndex = (index - 1 + total) % total;
+const nextIndex = (index + 1) % total;
+
+const prev = brandData[prevIndex];
+const next = brandData[nextIndex];
+
+// prev
+const prevEl = document.getElementById("prevBrand");
+prevEl.style.backgroundImage = `url(${prev.visualImage})`;
+prevEl.onclick = () => {
+  location.href = `brand.html?id=${prev.id}`;
+};
+
+// next
+const nextEl = document.getElementById("nextBrand");
+nextEl.style.backgroundImage = `url(${next.visualImage})`;
+nextEl.onclick = () => {
+  location.href = `brand.html?id=${next.id}`;
+};
+// ----------------------
+// AOS
+// ----------------------
+AOS.init();
---
+++ JS/brandProduct.js
@@ -0,0 +1,94 @@
+import { brandData } from "./brandData.js";
+
+/* ===============================
+  URL 파라미터
+  brand=coca-cola
+  product=2
+================================ */
+const params = new URLSearchParams(window.location.search);
+const brandId = params.get("id");
+const productIndex = Number(params.get("product")) || 0;
+
+const productNameEl = document.getElementById("activeProductName");
+const mainImageEl = document.getElementById("activeProductImage");
+const gridEl = document.getElementById("productGrid");
+const productDescEl = document.getElementById("activeProductDesc");
+const brandTabsEl = document.getElementById("brandTabs");
+
+// 데이터 찾기
+const brand = brandData.find(b => b.id === brandId);
+
+if (!brand) {
+  console.error("브랜드 데이터를 찾을 수 없습니다.");
+}
+let activeIndex = productIndex;
+
+function renderBrandTabs() {
+  brandTabsEl.innerHTML = "";
+
+  brandData.forEach(b => {
+    const btn = document.createElement("button");
+    btn.type = "button";
+    btn.className = "brand-tab";
+    btn.textContent = "#" + b.nameKR;
+
+    // 현재 페이지 브랜드면 active
+    if (b.id === brandId) {
+      btn.classList.add("active");
+    }
+
+    btn.addEventListener("click", () => {
+      // 브랜드만 변경 → 첫 제품으로
+      location.href = `brand-product.html?id=${b.id}&product=0`;
+    });
+
+    brandTabsEl.appendChild(btn);
+  });
+}
+
+
+// 메인 제품 변경 함수
+function setActiveProduct(index) {
+  activeIndex = index;
+  const product = brand.products[index];
+
+  productNameEl.textContent = product.name;
+  mainImageEl.src = product.image;
+  mainImageEl.alt = product.name;
+  productDescEl.textContent = product.productDescription?.detail || "";
+
+  updateActiveGrid();
+}
+
+// grid 생성
+function renderGrid() {
+  gridEl.innerHTML = "";
+
+  brand.products.forEach((product, index) => {
+    const button = document.createElement("button");
+    button.className = "product-card";
+    button.type = "button";
+
+    button.innerHTML = `
+      <img src="${product.image}" alt="${product.name}" />
+    `;
+
+    button.addEventListener("click", () => {
+      setActiveProduct(index);
+    });
+
+    gridEl.appendChild(button);
+  });
+}
+
+// active
+function updateActiveGrid() {
+  const items = document.querySelectorAll(".product-card");
+  items.forEach((item, index) => {
+    item.classList.toggle("active", index === activeIndex);
+  });
+}
+
+renderBrandTabs();
+renderGrid();
+setActiveProduct(activeIndex);
